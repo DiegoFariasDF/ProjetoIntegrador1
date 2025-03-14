393e3fcea3f7a8a8a66a393dfd358da949cb4afe
DiegoFariasDF <86974387+DiegoFariasDF@users.noreply.github.com>
2025-03-14 13:39:25 -0300

    Simples inclusão de icone para aba do navegado

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" href="public/css/style.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="/public/js/index.js"></script>
+    <link rel="icon" href="public/img/logoRedondoDark48.png" type="image/png">
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
