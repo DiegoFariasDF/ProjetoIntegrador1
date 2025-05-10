45eaf61818331f3cdfc50c304df9602d7886262e
DiegoFariasDF <diegofariaspl@gmail.com>
2025-05-10 20:23:32 -0300

    Bug na lista de emprestimos corrigido

--- app/views/emprestimo.php
+++ app/views/emprestimo.php
@@ -43,7 +43,7 @@
 </div>
 
     
-    <div class="table-responsive" style="max-height: 400px; overflow-y: auto;">
+<div class="table-responsive" style="max-height: 400px; overflow-y: auto;">
         <table class="table table-striped">
             <thead>
                 <tr>
@@ -56,12 +56,12 @@
             </thead>
             <tbody id="loan-list">
                 <?php if (!empty($emprestimos)): ?>
-                <?php foreach ($emprestimos as $emprestimo): ?>
-                    <tr data-status="<?= $emprestimo['status'] ?>">
-                        <td><?= htmlspecialchars($emprestimo['id']) ?></td>
-                        <td><?= htmlspecialchars($emprestimo['nome']) ?></td>
-                        <td><?= htmlspecialchars($emprestimo['livro']) ?></td>
-                        <td><?= htmlspecialchars($emprestimo['data_devolucao_formatada']) ?></td>
+                <?php foreach ($emprestimos as $emprestimos): ?>
+                    <tr data-status="<?= $emprestimos['status'] ?>">
+                        <td><?= htmlspecialchars($emprestimos['id']) ?></td>
+                        <td><?= htmlspecialchars($emprestimos['nome']) ?></td>
+                        <td><?= htmlspecialchars($emprestimos['livro']) ?></td>
+                        <td><?= htmlspecialchars($emprestimos['data_devolucao_formatada']) ?></td>
                         <td>
                             <!-- mobile -->
                             <div class="dropdown dropstart d-md-none">
@@ -69,17 +69,17 @@
                                     <i class="bi bi-gear"></i>
                                 </button>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item btn-renovar" href="?pagina=renovar_emprestimo&id=<?= $emprestimo['id'] ?>">Renovar</a></li>
-                                    <li><a class="dropdown-item btn-finalizar" href="?pagina=finalizar_emprestimo&id=<?= $emprestimo['id'] ?>">Finalizar</a></li>
-                                    <li><a class="dropdown-item" href="https://api.whatsapp.com/send/?phone=55<?= $emprestimo['telefone'] ?>&text=Ol%C3%A1%2C%20estou%20entrando%20em%20contato%20para%20lembrar%20que%20o%20livro%20<?= urlencode($emprestimo['livro']) ?>%20est%C3%A1%20com%20a%20devolu%C3%A7%C3%A3o%20atrasada.%20Por%20favor%2C%20devolva%20o%20mais%20r%C3%A1pido%20poss%C3%ADvel.%20Agrade%C3%A7o%21" target="_blank">Mensagem</a></li>
+                                    <li><a class="dropdown-item btn-renovar" href="?pagina=renovar_emprestimo&id=<?= $emprestimos['id'] ?>">Renovar</a></li>
+                                    <li><a class="dropdown-item btn-finalizar" href="?pagina=finalizar_emprestimo&id=<?= $emprestimos['id'] ?>">Finalizar</a></li>
+                                    <li><a class="dropdown-item" href="https://api.whatsapp.com/send/?phone=55<?= $emprestimos['telefone'] ?>&text=Ol%C3%A1%2C%20estou%20entrando%20em%20contato%20para%20lembrar%20que%20o%20livro%20<?= urlencode($emprestimo['livro']) ?>%20est%C3%A1%20com%20a%20devolu%C3%A7%C3%A3o%20atrasada.%20Por%20favor%2C%20devolva%20o%20mais%20r%C3%A1pido%20poss%C3%ADvel.%20Agrade%C3%A7o%21" target="_blank">Mensagem</a></li>
                                 </ul>
                             </div>
 
                             <!-- desktop -->
                             <div class="d-none d-md-flex gap-2">
-                                <a class="btn btn-secondary btn-sm btn-renovar" href="?pagina=renovar_emprestimo&id=<?= $emprestimo['id'] ?>">Renovar</a>
-                                <a class="btn btn-dark btn-sm btn-finalizar" href="?pagina=finalizar_emprestimo&id=<?= $emprestimo['id'] ?>">Finalizar</a>
-                                <a class="btn btn-success btn-sm" href="https://api.whatsapp.com/send/?phone=55<?= $emprestimo['telefone'] ?>&text=Ol%C3%A1%2C%20estou%20entrando%20em%20contato%20para%20lembrar%20que%20o%20livro%20<?= urlencode($emprestimo['livro']) ?>%20est%C3%A1%20com%20a%20devolu%C3%A7%C3%A3o%20atrasada.%20Por%20favor%2C%20devolva%20o%20mais%20r%C3%A1pido%20poss%C3%ADvel.%20Agrade%C3%A7o%21" target="_blank">Mensagem</a>
+                                <a class="btn btn-secondary btn-sm btn-renovar" href="?pagina=renovar_emprestimo&id=<?= $emprestimos['id'] ?>">Renovar</a>
+                                <a class="btn btn-dark btn-sm btn-finalizar" href="?pagina=finalizar_emprestimo&id=<?= $emprestimos['id'] ?>">Finalizar</a>
+                                <a class="btn btn-success btn-sm" href="https://api.whatsapp.com/send/?phone=55<?= $emprestimos['telefone'] ?>&text=Ol%C3%A1%2C%20estou%20entrando%20em%20contato%20para%20lembrar%20que%20o%20livro%20<?= urlencode($emprestimo['livro']) ?>%20est%C3%A1%20com%20a%20devolu%C3%A7%C3%A3o%20atrasada.%20Por%20favor%2C%20devolva%20o%20mais%20r%C3%A1pido%20poss%C3%ADvel.%20Agrade%C3%A7o%21" target="_blank">Mensagem</a>
                             </div>
                         </td>
                     </tr>
